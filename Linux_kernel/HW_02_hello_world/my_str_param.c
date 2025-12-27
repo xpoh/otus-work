20a2d704cabfcc26193565e3e07f64572b4ab589
GitLab CI <gitlab-ci@example.com>
2025-12-27 22:54:22 +0300

    [F] kernel: work 2 fix

--- Linux_kernel/HW_02_hello_world/my_str_param.c
+++ Linux_kernel/HW_02_hello_world/my_str_param.c
@@ -12,28 +12,13 @@ MODULE_LICENSE("GPL");
 
 char my_str_param[MY_STR_MAX_SIZE] = "";
 
-static int my_str_param_set(const char *val, const struct kernel_param *kp)
-{
-	size_t len = strlen(val);
-	if (len >= MY_STR_MAX_SIZE) {
-		pr_err("string_val input string too long\n");
-		return -EINVAL;
-	}
-
-	strncpy(my_str_param, val, MY_STR_MAX_SIZE - 1);
-	my_str_param[MY_STR_MAX_SIZE - 1] = '\0';
-
-	pr_info("string_val value = %s\n", my_str_param);
-	return 0;
-}
-
 static int my_str_param_get(char *buf, const struct kernel_param *kp)
 {
 	return sprintf(buf, "%s", my_str_param);
 }
 
 static const struct kernel_param_ops my_str_params = {
-	.set = my_str_param_set,
+	.set = NULL,
 	.get = my_str_param_get,
 };
 
